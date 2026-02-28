d8bdada1c92f845992b62b3619978c8fead562ad
maearapowre-cmd <maearapowre@gmail.com>
2026-02-28 21:32:20 +0100

    Update payload.sh to use git for cloning bot.py
    Replaced curl command with git clone for downloading the bot script.

--- payload.sh
+++ payload.sh
@@ -1,3 +1,5 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
-curl https://github.com/maearapowre-cmd/bot.py
+sudo apt install git
+git clone https://github.com/maearapowre-cmd/bot.py
+cd maearapowre-cmd
 sudo python3 bot.py
