0db2da76e9e5f2d33f3a38a68d0f469865997e29
maearapowre-cmd <maearapowre@gmail.com>
2026-03-01 09:06:15 +0100

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,4 +1,4 @@
 sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
-curl https://raw.githubusercontent.com/maearapowre-cmd/payload/
+curl https://raw.githubusercontent.com/maearapowre-cmd/payload/main/bot.py
 cd maearapowre-cmd
 sudo python3 bot.py
